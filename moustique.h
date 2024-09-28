7398297da5b123a46caba20e06b14269fd490aaa
Bebel <abel37@free.fr>
2024-09-28 14:57:26 +0200

    Implementation scarabe, maj sauterelle

--- moustique.h
+++ moustique.h
@@ -7,11 +7,6 @@ class Moustique : public Insecte
 {
 public:
     Moustique(Team team);
-
-    Insecte* get_en_dessous() const { return en_dessous; }
-
-private:
-    Insecte* en_dessous = nullptr;
 };
 
 #endif // MOUSTIQUE_H
