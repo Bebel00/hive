bcd380d031574027b6d70704a6bde5d9602328e8
judithlajouanie <judith.lajouanie@etu.utc.fr>
2024-11-09 15:13:20 +0100

    Update araignee.cpp

--- araignee.cpp
+++ araignee.cpp
@@ -1,8 +1,34 @@
 #include "araignee.h"
-#include "insecte.h"
-#include "teams.h"
+#include "case.h"
+#include "plateau.h"
+#include <set>
 
-Araignee::Araignee(Team team) : Insecte(team)
-{
+Araignee::Araignee(Team team) : Insecte(team) { }
 
+void Araignee::get_moves_possibles(std::vector<Case*>& move_possibles) const {
+    std::set<Case*> visited;
+    std::vector<Case*> path;
+
+    auto dfs = [&](Case* current_case, int depth, auto&& dfs_ref) -> void {
+        if (depth == 3) {
+            if (Case::is_empty(current_case) && !move_casse_ruche(get_case(), get_case()->get_plateau()->get_cases())) {
+                move_possibles.push_back(current_case);
+            }
+            return;
+        }
+
+        visited.insert(current_case);
+        for (auto direction : Case::DIRECTIONS_ALL) {
+            Case* next_case = current_case->get_case_from_direction(direction);
+            if (next_case && visited.find(next_case) == visited.end() && Case::is_empty(next_case)) {
+                path.push_back(next_case);
+                dfs_ref(next_case, depth + 1, dfs_ref);
+                path.pop_back();
+            }
+        }
+        visited.erase(current_case);
+    };
+
+    dfs(get_case(), 0, dfs);
 }
+
