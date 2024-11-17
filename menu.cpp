80dc4e211031c7e69db5df70cbca306d3c3912fb
judithlajouanie <judith.lajouanie@etu.utc.fr>
2024-11-17 13:53:35 +0100

    Create menu.cpp

---
+++ menu.cpp
@@ -0,0 +1,55 @@
+#include "mainmenu.h"
+#include <QGraphicsRectItem>
+#include <QPushButton>
+#include <QGraphicsProxyWidget>
+
+MainMenu::MainMenu(QWidget* parent) : QGraphicsView(parent) {
+    mainMenuScene = new QGraphicsScene(this);
+    newGameScene = new QGraphicsScene(this);
+
+    setupMainMenu();
+    setupNewGameScene();
+
+    setScene(mainMenuScene);
+}
+
+void MainMenu::setupMainMenu() {
+    QGraphicsTextItem* title = new QGraphicsTextItem("Menu Principal");
+    title->setFont(QFont("Arial", 24));
+    title->setPos(100, 50);
+    mainMenuScene->addItem(title);
+
+    QPushButton* newGameButton = new QPushButton("Nouvelle Partie");
+    QGraphicsProxyWidget* newGameButtonProxy = mainMenuScene->addWidget(newGameButton);
+    newGameButtonProxy->setPos(100, 150);
+
+    QPushButton* quitButton = new QPushButton("Quitter");
+    QGraphicsProxyWidget* quitButtonProxy = mainMenuScene->addWidget(quitButton);
+    quitButtonProxy->setPos(100, 200);
+
+    connect(newGameButton, &QPushButton::clicked, this, &MainMenu::goToNewGame);
+    connect(quitButton, &QPushButton::clicked, this, &MainMenu::quitApplication);
+}
+
+void MainMenu::setupNewGameScene() {
+    QGraphicsTextItem* newGameTitle = new QGraphicsTextItem("Nouvelle Partie");
+    newGameTitle->setFont(QFont("Arial", 24));
+    newGameTitle->setPos(100, 50);
+    newGameScene->addItem(newGameTitle);
+
+    QPushButton* backButton = new QPushButton("Retour");
+    QGraphicsProxyWidget* backButtonProxy = newGameScene->addWidget(backButton);
+    backButtonProxy->setPos(100, 150);
+
+    connect(backButton, &QPushButton::clicked, [=]() {
+        setScene(mainMenuScene);
+    });
+}
+
+void MainMenu::goToNewGame() {
+    setScene(newGameScene);
+}
+
+void MainMenu::quitApplication() {
+    QApplication::quit();
+}
