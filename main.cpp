708c6e43e9bb9ced6ea8724104542ac54fd3c1df
judithlajouanie <judith.lajouanie@etu.utc.fr>
2024-11-17 15:47:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,44 @@
 #include "mainwindow.h"
+#include "mainmenu.h"
 #include "partie.h"
 #include <QApplication>
 #include <iostream>
 #include <string>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+
+    MainMenu menu;
+
     MainWindow w;
-    // w.show();
+    Partie* partie = nullptr;
 
-    Partie partie("Bebel", "Léo");
-    w.setPartie(&partie);
+    // Connexion pour lancer une nouvelle partie depuis le menu
+    QObject::connect(&menu, &MainMenu::nouvellePartieDemandee, [&]() {
+        // Création d'une nouvelle partie si celle ci est nécessaire
+        if (!partie) {
+            partie = new Partie("Leo", "Abel"); 
+            w.setPartie(partie);
+        }
 
-    while (true)
-    {
+        //pour cacher le menu et afficher la fenêtre principale
+        menu.hide();
+        w.show();
+    });
+
+    // connexion pour quitter l'application depuis le menu
+    QObject::connect(&menu, &MainMenu::quitApplication, [&]() {
+        a.quit();
+    });
+
+    // pour lancer  le menu principal
+    menu.show();
+
+   
+    while (partie) {
         std::string cmd;
         std::getline(std::cin, cmd);
-        std::cout << partie.jouer_tour_cli(cmd) << std::endl;
+        std::cout << partie->jouer_tour_cli(cmd) << std::endl;
     }
 
     return a.exec();
