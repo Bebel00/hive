a4deaf2273d066a0b35b48764893acb7497f7602
Nitnelav08 <valentin.carre08@gmail.com>
2024-11-24 20:55:51 +0100

    Add files via upload

--- abeille.cpp
+++ abeille.cpp
@@ -8,6 +8,8 @@ Abeille::Abeille(Team team) : Insecte(team)
 
 }
 
+
+// méthode permettant d'obtenir les déplacements possibles de l'abeille.
 void Abeille::get_moves_possibles(std::vector<Case *> &move_possibles) const
 {
     if (!move_casse_ruche(get_case(), get_case()->get_plateau()->get_cases()))
@@ -17,10 +19,11 @@ void Abeille::get_moves_possibles(std::vector<Case *> &move_possibles) const
             Case* c = get_case()->get_case_from_direction(i_direction);
             if (Case::is_empty(c))
             {
+                // Nous devons vérifier que chaque déplacement possible est un glissement et non pas un saut.
                 bool a_insecte_environnant = false;
                 for (auto j_direction : Case::DIRECTIONS_ALL)
                 {
-                    if (i_direction != Case::DIRECTION_OPPOSE(j_direction))
+                    if (i_direction != Case::DIRECTION_OPPOSE(j_direction)) // Nous vérifions que nous ne faisons pas de demi tour.
                     {
                         if (!Case::is_empty(c->get_case_from_direction(j_direction)))
                         {
