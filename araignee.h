ff96806207c1903126b4fd80b53f0f2f92593b1d
Bebel00 <lenekodunet@gmail.com>
2024-12-15 13:40:57 +0100

    Fourmi, araignee, alentours bug fix

--- araignee.h
+++ araignee.h
@@ -8,7 +8,7 @@ class Araignee : public Insecte
 public:
     Araignee(Team team) : Insecte(team) { }
 
-    virtual void get_moves_possibles(std::vector<Case*>& move_possibles)const   override;
+    virtual void get_moves_possibles(std::vector<Case*>& move_possibles) const override;
     virtual Type::Type get_type() const override { return Type::Type::ARAIGNEE; }
     virtual std::string get_chemin_icone() const override { return ""; }
 
