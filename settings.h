21cbabc54492bf0d809a93b3f587df26e9b73a9d
judithlajouanie <judith.lajouanie@etu.utc.fr>
2024-12-17 19:48:08 +0100

    Update settings.h

--- settings.h
+++ settings.h
@@ -4,6 +4,8 @@
 #include <QGraphicsView>
 #include <QGraphicsScene>
 #include <QObject>
+#include <QString>
+#include <QSet>
 
 class Settings : public QGraphicsView {
     Q_OBJECT
@@ -11,13 +13,26 @@ public:
     explicit Settings(QWidget* parent = nullptr);
 
 signals:
-    void saveSettings(QString joueur1, QString joueur2, bool contreIA);
+/**
+     * Signal pour sauvegarder les paramètres de la partie.
+     * @param joueur1 Le nom du Joueur 1.
+     * @param joueur2 Le nom du Joueur 2 ou "IA" si contre l'IA.
+     * @param contreIA Booléen pour savoir si l'adversaire est l'IA.
+     * @param extensions Un ensemble des extensions sélectionnées.
+     * @param nbUndo Le nombre de tours annulables.
+     */
+    void saveSettings(QString joueur1, QString joueur2, bool contreIA,QSet<QString> extensions, int nbUndo);
     void backToMainMenu(); // Signal pour retourner au menu principal
 
 private:
     QGraphicsScene* settingsScene=nullptr;
 
     void setupSettingsScene(); // Configure la scène des paramètres
+
+    void setupPlayerInputs();      // entrées pour les joueurs
+    void setupExtensionOptions();  //cases à cocher pour les extensions
+    void setupUndoOption();        //le champ pour les tours annulables
+    void setupButtons();           //les boutons Retour et Sauvegarde
 };
 
 #endif // SETTINGS_H
