9a3d9e8e6e751587f5019380ca23858e64559f0a
judithlajouanie <judith.lajouanie@etu.utc.fr>
2024-12-17 16:51:35 +0100

    Update araignee.h

--- araignee.h
+++ araignee.h
@@ -11,6 +11,7 @@ public:
     virtual Type::Type get_type() const override { return Type::Type::ARAIGNEE; }
     virtual std::string get_chemin_icone() const override { return ""; }
     virtual void get_moves_possibles(std::vector<Case*>& move_possibles) const override;
+    virtual int get_jetons_max() const override { return 2; }
 };
 
 #endif // ARAIGNEE_H
