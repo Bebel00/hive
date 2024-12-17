2ba85dae09bbc3de74081a49d4b173a3eedf7278
judithlajouanie <judith.lajouanie@etu.utc.fr>
2024-12-17 16:48:35 +0100

    Update abeille.h

--- abeille.h
+++ abeille.h
@@ -17,6 +17,8 @@ public:
     virtual std::string get_chemin_icone() const override { return ""; }
     virtual void get_moves_possibles(std::vector<Case *> &move_possibles) const override;
 
+    virtual int get_jetons_max() const override { return 1; }
+
     static bool enregistre;
 };
 
